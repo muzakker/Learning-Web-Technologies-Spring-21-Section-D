d385672f06970b8bd533b437e1b6a7575d408144
muzakker <muzakkerh@gmail.com>
2021-02-16 01:51:05 +0600

    comment removed

--- MID_LAB_TASK_PHP_04/task_1.php
+++ MID_LAB_TASK_PHP_04/task_1.php
@@ -6,7 +6,6 @@
 	$area = ($length * $width);
 	$perimeter = 2 * ($length + $width);
 	
-	//echo "Area for the Rectangle of length ".$length." and width ".$width" is: ".$area."\n";
 	echo "Area for the Rectangle of length ".$length." and width ".$width." is: ".$area."<br>";
 	echo "Perimeter for the Rectangle of length ".$length." and width ".$width." is: ".$perimeter;
 	
