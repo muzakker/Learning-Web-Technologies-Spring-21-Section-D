4ddb6763d5ca0740cd0c2dc6827761c6b36e3030
muzakker <muzakkerh@gmail.com>
2021-02-16 01:19:18 +0600

    final

--- MID_LAB_TASK_PHP_04/task_8.php
+++ MID_LAB_TASK_PHP_04/task_8.php
@@ -1,16 +1,34 @@
 <?php
 
 	$elements = [
-					[1, 2, 3, 'A'],
-					[1, 2, 'B', 'C'],
-					[1, 'D', 'E', 'F']
-				];
-	$elements_num = count($elements);
+				[1, 2, 3, 'A'],
+				[1, 2, 'B', 'C'],
+				[1, 'D', 'E', 'F']
+			];
 	
-	for($row = $elements_num; $row > 0; $row--){
-		for($col = $row; $col > 0; $col--){
+	$elements_no = count($elements);
+	
+	for($row = 0; $row < $elements_no; $row++){
+		$row_elements = count($elements[$row]);
+		for($col = 0; $col < $row_elements; $col++){
+			if($elements[$row][$col] >= 'A' && $elements[$row][$col] <= 'Z'){
+				break;
+			}
 			echo $elements[$row][$col];
 		}
+		echo "\n";
+	}
+	
+	echo "\n";
+	
+	for($row = 0; $row < $elements_no; $row++){
+		$row_elements = count($elements[$row]);
+		for($col = 0; $col < $row_elements; $col++){
+			if($elements[$row][$col] >= 'A' && $elements[$row][$col] <= 'Z'){
+				echo $elements[$row][$col];
+			}
+		}
+		echo "\n";
 	}
 	
 ?>
