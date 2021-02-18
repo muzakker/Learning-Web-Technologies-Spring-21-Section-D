7c6e5ec2b39702f68a75fcf0b77f779c12afd4ca
muzakker <muzakkerh@gmail.com>
2021-02-19 00:36:45 +0600

    TASK_6 -> TASK_B

---
+++ MID_LAB_TASK_PHP_05/TASK_6/TASK_B/bg.php
@@ -0,0 +1,39 @@
+<?php
+
+	if(isset($_POST['submit'])){
+		$bg = $_POST['bloodGroup'];
+		
+		echo "<b>BLOOD GROUP: </b>".$bg;
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>HTML Form using PHP</title>
+</head>
+
+<body>
+    <form method="POST" action="#">
+        <fieldset style="width:300px">
+            <legend><b>BLOOD GROUP</b></legend>
+            <select name="bloodGroup" >
+				<option value="A+" selected>A+</option>
+				<option value="A-">A-</option>
+				<option value="B+">B+</option>
+				<option value="B-">B-</option>
+				<option value="AB+">AB+</option>
+				<option value="AB-">AB-</option>
+				<option value="O+">O+</option>
+				<option value="O-">O-</option>
+			</select>
+			<hr>
+			<input type="submit" name="submit" value="Submit">
+        </fieldset>
+        <br>
+    </form>    
+</body>
+
+</html>
