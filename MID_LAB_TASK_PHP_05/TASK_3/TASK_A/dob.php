e80300340f460e5d2c220e817fe23d6d2f42a5d5
muzakker <muzakkerh@gmail.com>
2021-02-18 21:31:26 +0600

    TASK_3 -> TASK_A

---
+++ MID_LAB_TASK_PHP_05/TASK_3/TASK_A/dob.php
@@ -0,0 +1,19 @@
+<?php
+
+	if(isset($_POST['submit'])){
+		$day = $_POST['dd'];
+		$month = $_POST['mm'];
+		$year = $_POST['yyyy'];
+		
+		if($day == "" || $month == "" || $year == ""){
+			echo "null submission...";
+		}else if(($day > 0 && $day <= 31) && ($month > 0 && $day <= 12) && ($year > 1900 && $year < 2021)){
+			echo "<b>DATE OF BIRTH: </b>".$day." / ".$month." / ".$year;
+		}else{
+			echo "Please put a valid date (i.e., dd: 1-31, mm: 1-12, yyyy: 1900-2021)";
+		}
+	}else{
+		echo "invalid submission";
+	}
+
+?>
