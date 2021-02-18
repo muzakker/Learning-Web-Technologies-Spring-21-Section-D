45d7d2d3b72faf1285869697bb2afef7d2a596b0
muzakker <muzakkerh@gmail.com>
2021-02-18 21:05:52 +0600

    TASK_2 -> TASK_A

---
+++ MID_LAB_TASK_PHP_05/TASK_2/TASK_A/email.php
@@ -0,0 +1,13 @@
+<?php
+
+	if(isset($_POST['submit'])){
+		$email = $_POST['email'];
+		
+		if($email == ""){
+			echo "null submission...";
+		}else{
+			echo "<b>Email: </b>".$email;
+		}
+	}
+
+?>
