59d4dfc1404500f7ca81744c47454a78d07be98b
muzakker <muzakkerh@gmail.com>
2021-02-24 08:42:39 +0600

    publicHome.php

---
+++ MID_LAB_TASK_PHP_SESSION_07/publicHome.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Public Home</title>
+</head>
+
+<body>
+	<table border="1" width="100%">
+		<tr>
+			<td>
+				<img src="logo.png" alt="logo" width="100px" height="100px">
+			</td>
+			
+			<td align="right">
+				<a href="#">Home</a> | <a href="login.php">Login</a> | <a href="registration.php">Registration</a>
+			</td>
+		</tr>
+		
+		<tr style="height:150px;">
+			<td colspan="2">
+				Welcome to xCompany
+			</td>
+		</tr>
+		
+		<tr>
+			<td colspan="2" align="center">
+				Copyright &#169; 2017
+			</td>
+		</tr>			
+	</table>
+</body>
+
+</html>
